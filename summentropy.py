0c736d8f977da0c70c0539acd3b90b6841ce2455
aogavrilov <55056138+aogavrilov@users.noreply.github.com>
2019-11-29 21:57:48 +0300

    Delete summentropy.py

--- summentropy.py
+++
@@ -1,7 +0,0 @@
-import math
-mm=[0.0078125,0.0078125,0.0078125,0.03125,0.296875,0.6484375]
-summentropy = 0;
-for x in mm:
-    summentropy-=x*math.log2(x)
-    print(x*math.log2(x))
-print(summentropy)
