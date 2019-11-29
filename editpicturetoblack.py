b076c16911ebd9b7663f30bbb37beed8709b0623
aogavrilov <55056138+aogavrilov@users.noreply.github.com>
2019-11-29 21:57:04 +0300

    Delete editpicturetoblack.py

--- editpicturetoblack.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import cv2
-img = cv2.imread("key-flat-128x128.png",0)
-for i in range(128):
-    for j in range(128):
-        img[i][j] = (img[i][j]//20)*20
-cv2.imwrite('key-flag-black.png', img)
